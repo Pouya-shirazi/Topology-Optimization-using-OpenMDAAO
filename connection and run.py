8405c744a056f209a8f1e8ae6fdb5de8752be709
Pouya-shirazi <130832757+Pouya-shirazi@users.noreply.github.com>
2023-04-20 16:49:47 +0330

    Update connection and run.py
    I edited the connection file and now it runs, But the results are not as expected. 
    I'll keep working on finding the issue.

--- connection and run.py
+++ connection and run.py
@@ -1,32 +1,41 @@
-from openmdao.api import Problem, IndepVarComp, ScipyOptimizeDriver
+from openmdao.api import Problem, IndepVarComp, ScipyOptimizeDriver, Group
 import openmdao.api as om
+# change directory to the path containing the disciplines
+
+# import the disciplines
 import Disc0
 import Disc1
 import Disc2
 import Disc3
 
-class MyModel(om.Group):
+class MyModel(Group):
     def setup(self):
         # Define the disciplines
-        self.add_subsystem('Disc1', Disc1(), promotes_inputs=['xy00'], promotes_outputs=['H'])
-        self.add_subsystem('Disc2', Disc2(), promotes_inputs=['H'], promotes_outputs=['dvdh'])
-        self.add_subsystem('Disc3', Disc3(), promotes_inputs=['H'], promotes_outputs=['comp'])
-        
-        # Define the connections
-        self.connect('Disc1.H', 'Disc2.H')
-        self.connect('Disc1.H', 'Disc3.H')
-        
-        # Set up the optimizer
-        self.add_design_var('Disc1.xy00')
-        self.add_objective('Disc3.comp')
+        d1 = Disc1.disc1()
+        d2 = Disc2.disc2()
+        d3 = Disc3.disc3()
+
+        self.add_subsystem('Disc1', d1, promotes_inputs=['xy00'], promotes_outputs=['H'])
+        self.add_subsystem('Disc2', d2, promotes_inputs=['H'], promotes_outputs=['dvdh'])
+        self.add_subsystem('Disc3', d3, promotes_inputs=['H'], promotes_outputs=['Comp'])
+
 
 prob = om.Problem()
 prob.model = MyModel()
 
-# Run the optimization
 prob.driver = om.ScipyOptimizeDriver()
 prob.driver.options['optimizer'] = 'SLSQP'
-prob.driver.options['tol'] = 1e-6
+
+prob.model.add_design_var('xy00')
+prob.model.add_objective('Comp')
+prob.model.add_constraint('dvdh', lower=0.0)
 
 prob.setup()
+prob.set_solver_print(level=0)
+
+#prob.set_val('xy00', np.array([0.0, 0.0])
+
 prob.run_driver()
+
+print('Minimum Compliance =', prob.get_val('Comp'))
+print('Optimal Design =', prob.get_val('H'))
